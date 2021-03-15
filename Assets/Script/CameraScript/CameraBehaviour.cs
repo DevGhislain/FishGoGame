45031b25f88e051c41f617856cef711d4f288946
zanze ghislain <zanzeghislain1@gmail.com>
2021-03-15 22:12:27 +0000

    Implementation of mouvement the Character and cam

---
+++ Assets/Script/CameraScript/CameraBehaviour.cs
@@ -0,0 +1,36 @@
+﻿// Using Unity System
+using UnityEngine;
+
+// Script for the Camera
+public class CameraBehaviour : MonoBehaviour
+{
+    #region private Accessor Unity
+
+    /// <summary>
+    /// Reference of the Character
+    /// </summary>
+    public GameObject Character;
+
+    #endregion
+
+    #region Public Accessor Unity
+
+    /// <summary>
+    /// Get the first method for Update
+    /// </summary>
+    private void Awake()
+    {
+       
+    }
+
+
+    /// <summary>
+    /// Method principale for the Update
+    /// </summary>
+    private void Update()
+    {
+        transform.position = new Vector3(Character.transform.position.x + 5.0f, 0, -10);
+    }
+
+    #endregion
+}
---
+++ Assets/Script/Charactere/CharacterBehaviour.cs
@@ -0,0 +1,172 @@
+﻿// Using Unity System
+using System.Collections;
+using UnityEngine;
+
+// Script for the Character behaviour
+public class CharacterBehaviour : MonoBehaviour
+{
+    #region private Accessor Unity
+
+    /// <summary>
+    /// Reference of the Rigibody for the character 
+    /// </summary>
+    [SerializeField]
+    private Rigidbody2D rigidbody2D;
+
+    /// <summary>
+    /// Reference of the vulnerability for the character 
+    /// </summary>
+    [SerializeField]
+    private float vulnerabilityTime = 5.5f;
+
+    #endregion
+
+
+    #region Private Members
+
+
+
+    /// <summary>
+    /// Reference for the character if is grounded
+    /// </summary>
+    private bool IsGrounded = false;
+
+    #endregion
+
+    #region Public Membres
+
+   /// <summary>
+    ///  Get the reference for the rigidbody for the character
+    /// </summary>
+    public Rigidbody2D rbd2D
+    {
+        get
+        {
+            return rigidbody2D;
+        }
+    }
+
+    /// <summary>
+    ///  Get the  reference for the character if is grounded
+    /// </summary>
+    public bool isGrounded
+    {
+        set
+        {
+            IsGrounded = value;
+        }
+
+        get
+        {
+            return IsGrounded;
+        }
+    }
+
+    /// <summary>
+    /// Reference of the speed Character
+    /// </summary>
+    ///Use the serielzefield
+    public float speedForCharacter;
+
+    /// <summary>
+    /// Reference of the speed Character
+    /// </summary>
+    ///Use the serielzefield
+    public float MaxJump;
+
+    #endregion
+
+    #region Method for the Unity
+
+    /// <summary>
+    /// Get the first method for Update
+    /// </summary>
+    private void Start()
+    {
+        SetVelocity(speedForCharacter, 0);
+        
+    }
+
+    /// <summary>
+    /// Methods principale for the Update
+    /// </summary>
+    private void Update()
+    {
+        if (Input.GetKeyDown("space") && IsGrounded )
+        {
+            JumpCharacter(MaxJump);  
+        } 
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Method for the Jump Character
+    /// </summary>
+   public void JumpCharacter(float _valueJumpe)
+    {
+        rigidbody2D.velocity += new Vector2(0, _valueJumpe);
+        IsGrounded = false;
+    }
+
+    /// <summary>
+    /// Set the velocity
+    /// </summary>
+    /// <param name="_xVelocity"> X velocioty</param>
+    /// <param name="_yVelocity"> Y velocity</param>
+    void SetVelocity(float _xVelocity, float _yVelocity)
+    {
+        rigidbody2D.velocity = new Vector2(_xVelocity, _yVelocity);
+    }
+
+    /// <summary>
+    /// Reference of the OnCollisionEnter2D for the Plaform
+    /// </summary>
+    /// <param name="collisionEnterCharcater"></param>
+    void OnCollisionEnter2D(Collision2D collisionEnterCharcater)
+    {
+        if (collisionEnterCharcater.gameObject.CompareTag("platform"))
+        {
+            IsGrounded = true;
+        }
+    }
+
+ /*  /// <summary>
+    /// Reference of the OnCollisionExit2D for the Obstacle
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+           // StartCoroutine(GetObstacle());
+        }
+    }
+
+    /// <summary>
+    /// Use the Enumerator 
+    /// </summary>
+    /// <returns></returns>
+  IEnumerator GetObstacle()
+    {
+        // Stop the the player
+       // yield return new WaitForSeconds(0.1f);
+      //  SetVelocity(speedForCharacter / 2, 0);
+
+        //Attack for the Monster
+        //GameObject.FindWithTag("Monster").GetComponent<MonsterBehaviour>().GoCloser();
+
+        // start the player
+        //yield return new WaitForSeconds(0.5f);
+        //SetVelocity(speedForCharacter, 0);
+
+        // No Attack for the Monster
+        //yield return new WaitForSeconds(vulnerabilityTime);
+       // GameObject.FindWithTag("Monster").GetComponent<MonsterBehaviour>().GoFurther();
+
+    }
+   */
+    #endregion
+}
