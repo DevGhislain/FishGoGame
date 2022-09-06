2bf2c2440fcfab4e3e48e6c5e70bb4eec07c92f3
Zanze ghislain <77408865+zanze225@users.noreply.github.com>
2022-09-06 10:17:43 +0000

    Add new parametter

---
+++ Assets/Script/TestScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    GameObject dd; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (dd != null)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
